5913d4bf423a733db1a7df6207eaa4094c24e5c8
dike.zhang <dike.zhang@gmail.com>
2018-04-21 22:56:43 -0400

    add web.config

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ nodeServicePlan --runtime "NODE|8.1"
 
 # Deploy code from a public GitHub repository.
 az webapp deployment source config --name $webappname --resource-group nodeResourceGroup \
---repo-url $gitrepo --branch v1.0 --manual-integration
+--repo-url $gitrepo --branch master --manual-integration
 
 # Copy the result of the following command into a browser to see the web app.
 echo http://$webappname.azurewebsites.net
